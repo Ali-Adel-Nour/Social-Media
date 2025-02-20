5ae2079c51b866b37494fb1157ce7fed1a6c39e6
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-02-20 15:23:38 +0200

    second layer for validation for user input

---
+++ identity-service/utils/validation.js
@@ -0,0 +1,13 @@
+const joi = require('joi');
+
+const validateRegistartion=(data)=>{
+  const schema = joi.object({
+    username: joi.string().alphanum().min(3).max(30).required(),
+    password: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')).required(),
+    email: joi.string().email().required(),
+
+  });
+  return schema.validate(data);
+}
+
+module.exports = validateRegistartion;
