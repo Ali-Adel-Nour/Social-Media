2e59d6b8723cdfabb6d32ab56a28c6bfdbcde380
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-03-16 20:35:49 +0200

    feat (api-gateaway) building a api gateway proxy

--- api-gateaway/src/server.js
+++ api-gateaway/src/server.js
@@ -8,9 +8,16 @@ const Redis = require('ioredis')
 
 const helmet = require('helmet')
 
-const {rateLimit} = require('express-rate-limit')
+const rateLimit = require('express-rate-limit')
 
-const {RedisStore} = require('express-redis-store')
+
+const { RedisStore } = require("rate-limit-redis");
+
+const logger = require('../utils/logger')
+
+const proxy = require('express-http-proxy')
+
+const errorHandler = require('../middlware/errorHandler')
 
 const app = express()
 
@@ -24,32 +31,76 @@ app.use(cors())
 
 app.use(express.json())
 
+const generalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+    prefix: "rl:general:",
+  }),
+});
 
-const rateLimiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'middleware',
-  points: 10, // Number of points
-  duration: 1 // Per second
-})
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Auth rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many authentication attempts",
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+    prefix: "rl:auth:",
+  }),
+});
 
 
- const rateLimit = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50,
-    standardHeaders: true,
-    legacyHeaders: false,
-    handler: (req, res) => {
-      logger.warn(`Auth rate limit exceeded for IP: ${req.ip}`);
-      res.status(429).json({
-        success: false,
-        message: 'Too many authentication attempts'
-      });
+console.log('IDENTITY_SERVICE_URL:', process.env.IDENTITY_SERVICE_URL);
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/^\/v1/, "/api");
+  },
+  proxyErrorHandler: (err, res, next) => {
+    logger.error(`Proxy error: ${err.message}`);
+    res.status(500).json({
+      message: `Internal server error`,
+      error: err.message,
+    });
+  },
+};
+
+//setting up proxy for our identity service
+app.use(
+  "/v1/auth",
+  proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      return proxyReqOpts;
     },
-    store: new RedisStore({
-      sendCommand: (...args) => redisClient.call(...args),
-      prefix: 'rl:auth:'
-    })
-  });
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Identity service: ${proxyRes.statusCode}`
+      );
 
+      return proxyResData;
+    },
+  })
+);
 
   app.use(rateLimit);
+  app.use(errorHandler)
+
+
+  app.listen(PORT, () => {
+    logger.info(`API Gateaway is running on port ${PORT}`)
+    logger.info(`Identity Service is running on port ${process.env.IDENTITY_SERVICE_URL}`)
+    logger.info(`Redis is running on port ${process.env.Redis_URL}`)
+
+  })
